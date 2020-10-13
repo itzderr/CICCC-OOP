351711f8c387666de8de61fc9d0868560a0d8ee5
Derrick Park <derrick8689@gmail.com>
2020-10-13 16:08:31 -0700

    Add user interation, final and static, and lab1 starter

---
+++ src/lab1/Model.java
@@ -0,0 +1,36 @@
+package lab1;
+
+/**
+ * @author Derrick Park
+ */
+public class Model {
+    private String firstName;
+    private String lastName;
+    private int height;
+    private double weight;
+    private boolean canTravel;
+    private boolean smokes;
+
+    /**
+     * Set the weight in pounds
+     * @param pounds pounds (lb)
+     */
+    public void setWeight(double pounds) {
+
+    }
+
+    /**
+     * Returns the height in feet and inches
+     * @return the height in feet and inches
+     */
+    public String getHeightInFeetAndInches() {
+        return "";
+    }
+
+    /**
+     * Prints detail information about this Model
+     */
+    public void printDetails() {
+
+    }
+}
