36358aeb8517758254e0082eb9fac3f076b07cd4
YumiMachino <y.y.1515m@gmail.com>
2020-10-22 11:19:08 -0700

    Update

--- src/MiniProject/Question.java
+++ src/MiniProject/Question.java
@@ -9,19 +9,22 @@ public class Question {
     // data field
     private ArrayList<String> questions = new ArrayList<>();  // put all the cities in the arraylist
     private String question; // randomly chosen question
-    //    private char[] charQuestion; // char array after chosen question in string converted
-    private ArrayList<Character> previousCorrectGuesses = new ArrayList<>(); // Store user input (correct)
-    private ArrayList<Character> previousWrongGuesses = new ArrayList<>(); // Store user input (wrong)
-    private StringBuilder currentGuess = new StringBuilder();
-    private char charInput;
-    private String input;
+    private String convertedQuestion; // chosen city in all small letters
 
+    private String input;   // user input
+    private String convertedInput; // user input in all small letters
+    private char charInput; // user input in char type
+    private char convertedCharInput; // user char input in small letter
+    private StringBuilder currentGuess = new StringBuilder();
+    private ArrayList<Character> previousWrongGuesses = new ArrayList<>(); // Store user input (wrong)
+
+//    private ArrayList<Character> previousCorrectGuesses = new ArrayList<>(); // Store user input (correct)
 
 
     // constructor field // to set the initialized value(if I want user to set the initialized value)
     public Question() {
         this.previousWrongGuesses = previousWrongGuesses;
-        this.previousCorrectGuesses = previousCorrectGuesses;
+//        this.previousCorrectGuesses = previousCorrectGuesses;
         this.currentGuess = currentGuess;
     }
 
@@ -29,6 +32,8 @@ public class Question {
     public String getQuestion() {
         return question;
     }
+
+    public String getConvertedQuestion(){return convertedQuestion; }
 
     public ArrayList getQuestions() {
         return questions;
@@ -38,9 +43,9 @@ public class Question {
         return previousWrongGuesses;
     }
 
-    public ArrayList getPreviousCorrectGuesses() {
-        return previousCorrectGuesses;
-    }
+//    public ArrayList getPreviousCorrectGuesses() {
+//        return previousCorrectGuesses;
+//    }
 
     public StringBuilder getCurrentGuess() {
         return currentGuess;
@@ -54,9 +59,11 @@ public class Question {
         return input;
     }
 
+    public String getConvertedInput() {return convertedInput;}
+
+    public char getConvertedCharInput(){return convertedCharInput;}
 
 
-    //OK : Read file and store the city into ArrayList(question)
 
     /**
      * @return ArrayList questions
@@ -76,8 +83,6 @@ public class Question {
         return questions;
     }
 
-    // pick a random city, return char array of the string
-
     /**
      * @return String question
      */
@@ -85,49 +90,60 @@ public class Question {
         double random = (Math.random() * questions.size());
         long randomNum = Math.round(random);
         int i = (int) randomNum;
-        String question = questions.get(i);
+        String question = questions.get(i); // 大文字小文字ミックスされた状態
         this.question = question;
+        convertedQuestion = getQuestion().toLowerCase(); // 全て小文字に変換したもの
+        this.convertedQuestion = convertedQuestion;
         return question;
     }
 
     // Convert its letters to underscores, and display
-    public void displayQuestion() {
-        int length = question.length(); // length of chosen city
+    public  void displayQuestion() {
+        int length = getQuestion().length(); // length of chosen city
         for (int i = 0; i < length; i++) {
             char ch = question.charAt(i);
-            if(!"".equals(ch)) {
-                currentGuess.replace(i, i + 1, "_");
+            String str = Character.toString(ch);
+            String str1 = " ";
+            if(str.equals(str1)) {
+                currentGuess.replace(i, i + 1, " ");
             }
+            currentGuess.replace(i, i + 1, "_");
         }
-        System.out.println(currentGuess);
+        System.out.println(getCurrentGuess());
     }
-
 
     //    take user's input, search for it in the city (-> boolean)
     public boolean isInQuestion() {
         System.out.print("Guess a letter: ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        this.input = input;
-        //convert user input from string to char
-        char charInput = input.charAt(0);
+        this.input = input; // user input
+        convertedInput = getInput().toLowerCase(); // user input all small letter
+        this.convertedInput = convertedInput;
+        convertedCharInput = convertedInput.charAt(0);  // charとして小文字で指定
+        this.convertedCharInput = convertedCharInput;
+        char charInput = input.charAt(0);  // user input(char ver)
         this.charInput = charInput;
-        System.out.println("Your guess: " + charInput);
-        if (getQuestion().indexOf(input) == -1) {
-            return false;
-        } else {
+
+        System.out.println("Your guess: " + getInput());
+        if (getConvertedQuestion().contains(getConvertedInput())) {  //小文字同士で比較
             return true;
+        } else {
+            return false;
         }
     }
 
     // reveal correct letters and display (-> string)
-    public void displayGuess() {
+    public StringBuilder displayGuess() {
         for (int i = 0; i < getQuestion().length(); i++) {
-            if (question.charAt(i) == getCharInput()) {
-                currentGuess.replace(i, i + 1, getInput());
+            if (getConvertedQuestion().charAt(i) == getConvertedCharInput()) {  //小文字で比較
+                char ch1 = getQuestion().charAt(i);
+                String str = String.valueOf(ch1);
+                currentGuess.replace(i, i + 1, str);    //入れ替えを本題のquestion stringのiばんめの文字持ってくる
             }
         }
-        System.out.println(currentGuess);
+        this.currentGuess = currentGuess;
+        return currentGuess;
     }
 
     // logic to keep track of wrong letters so they don't lose points
