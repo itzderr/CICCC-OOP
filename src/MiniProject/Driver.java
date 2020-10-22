aac92e15b4fa69d358f7f0cb53fba10f1ab5304f
YumiMachino <y.y.1515m@gmail.com>
2020-10-22 11:50:38 -0700

    Update

--- src/MiniProject/Driver.java
+++ src/MiniProject/Driver.java
@@ -1,61 +1,40 @@
 package MiniProject;
 
-
 import java.util.Scanner;
 
 public class Driver {
 
     public static void main(String[] args) {
-        // start of the game
         Question q1 = new Question();
         q1.loadFile();
+//        q1.pickCity();
         System.out.println(q1.pickCity());
         q1.displayQuestion();  // shows all underscore bar
 
         while (!q1.isOver()) {
-            int wrongCount = q1.getPrevioudWrongGuesses().size(); // 毎回更新
             if (q1.isInQuestion()) {    // input char if in question
-                if (!q1.getCurrentGuess().toString().contains("_")) {     // if there are still unrevealed letter
+                if(q1.displayGuess().toString().contains("_")){
+                    System.out.println("You are guessing: " + q1.displayGuess());
+                    System.out.println("You have guessed (" + q1.getPreviousWrongGuesses().size() + ") wrong letters.");
+                }
+                else{
                     System.out.println("You win!\nYou have guessed '" + q1.getQuestion() + "' correctly");
                     System.exit(0);
                 }
-                System.out.println("You are guessing: " + q1.displayGuess()); // displayQuestion　q1.getCurrentGuess()
-                System.out.println("You have guessed (" + q1.getPrevioudWrongGuesses().size() + ") wrong letters.");
-
-
-//            } else if (!q1.isWrongBefore()) {
-//                //previously did not get same wrong char
-//                q1.getPrevioudWrongGuesses().add(q1.getCharInput()); //以前間違えてなければそのcharを追加
-//                if (q1.getPrevioudWrongGuesses().size() < 10) {
-////                    System.out.println(q1.getPrevioudWrongGuesses());
-//                    System.out.println("You are guessing: " + q1.getCurrentGuess());
-//                    System.out.println("You have guessed (" + q1.getPrevioudWrongGuesses().size() + ") wrong letters.");
-//                }
-//
-//                if (q1.getPrevioudWrongGuesses().size()== 10) {
-//                    System.out.println("You lose!\nThe correct word was '" + q1.getQuestion() + "' !");
-//                    System.exit(0);
-//                }
-//            } else {
-//                System.out.println("You are guessing: " + q1.getCurrentGuess());
-//                System.out.println("You have guessed (" + q1.getPrevioudWrongGuesses().size() + ") wrong letters.");
-//            }
             }else if (q1.isWrongBefore()){
-                    System.out.println(q1.getPrevioudWrongGuesses());
-                    if (q1.getPrevioudWrongGuesses().size() == 10){
+                    if (q1.getPreviousWrongGuesses().size() == 10){
                         System.out.println("You lose!\nThe correct word was '" + q1.getQuestion() + "' !");
                         System.exit(0);
                     }
                     else {
                         System.out.println("You are guessing: " + q1.getCurrentGuess());
-                        System.out.println("You have guessed (" + q1.getPrevioudWrongGuesses().size() + ") wrong letters.");
+                        System.out.println("You have guessed (" + q1.getPreviousWrongGuesses().size() + ") wrong letters.");
                     }
             } else {
-                    q1.getPrevioudWrongGuesses().add(q1.getCharInput()); //以前間違えてなければそのcharを追加
-                    if(q1.getPrevioudWrongGuesses().size() < 10){
-                        System.out.println(q1.getPrevioudWrongGuesses());
+                    q1.getPreviousWrongGuesses().add(q1.getCharInput());            //以前間違えてなければそのcharを追加
+                    if(q1.getPreviousWrongGuesses().size() < 10){
                         System.out.println("You are guessing: " + q1.getCurrentGuess());
-                        System.out.println("You have guessed (" + q1.getPrevioudWrongGuesses().size() + ") wrong letters.");
+                        System.out.println("You have guessed (" + q1.getPreviousWrongGuesses().size() + ") wrong letters.");
                     } else{
                         System.out.println("You lose!\nThe correct word was '" + q1.getQuestion() + "' !");
                         System.exit(0);
--- src/MiniProject/Question.java
+++ src/MiniProject/Question.java
@@ -6,46 +6,38 @@ import java.util.Scanner;
 
 
 public class Question {
-    // data field
-    private ArrayList<String> questions = new ArrayList<>();  // put all the cities in the arraylist
-    private String question; // randomly chosen question
-    private String convertedQuestion; // chosen city in all small letters
-
-    private String input;   // user input
-    private String convertedInput; // user input in all small letters
-    private char charInput; // user input in char type
-    private char convertedCharInput; // user char input in small letter
+    private ArrayList<String> questions = new ArrayList<>();                  // put all the cities in the arraylist
+    private String question;                                                  // randomly chosen question
+    private String convertedQuestion;                                         // chosen city in all small letters
+    private String input;                                                     // user input
+    private String convertedInput;                                            // user input in all small letters
+    private char charInput;                                                   // user input in char type
+    private char convertedCharInput;                                          // user char input in small letter
     private StringBuilder currentGuess = new StringBuilder();
-    private ArrayList<Character> previousWrongGuesses = new ArrayList<>(); // Store user input (wrong)
-
-//    private ArrayList<Character> previousCorrectGuesses = new ArrayList<>(); // Store user input (correct)
+    private ArrayList<Character> previousWrongGuesses = new ArrayList<>();    // Store user input (wrong)
 
 
-    // constructor field // to set the initialized value(if I want user to set the initialized value)
     public Question() {
         this.previousWrongGuesses = previousWrongGuesses;
-//        this.previousCorrectGuesses = previousCorrectGuesses;
         this.currentGuess = currentGuess;
     }
 
-    // getter
+    // getter methods
     public String getQuestion() {
         return question;
     }
 
-    public String getConvertedQuestion(){return convertedQuestion; }
+    public String getConvertedQuestion(){
+        return convertedQuestion;
+    }
 
     public ArrayList getQuestions() {
         return questions;
     }
 
-    public ArrayList getPrevioudWrongGuesses() {
+    public ArrayList getPreviousWrongGuesses() {
         return previousWrongGuesses;
     }
-
-//    public ArrayList getPreviousCorrectGuesses() {
-//        return previousCorrectGuesses;
-//    }
 
     public StringBuilder getCurrentGuess() {
         return currentGuess;
@@ -59,12 +51,15 @@ public class Question {
         return input;
     }
 
-    public String getConvertedInput() {return convertedInput;}
+    public String getConvertedInput() {
+        return convertedInput;
+    }
 
-    public char getConvertedCharInput(){return convertedCharInput;}
+    public char getConvertedCharInput(){
+        return convertedCharInput;
+    }
 
-
-
+    // methods
     /**
      * @return ArrayList questions
      */
@@ -90,21 +85,23 @@ public class Question {
         double random = (Math.random() * questions.size());
         long randomNum = Math.round(random);
         int i = (int) randomNum;
-        String question = questions.get(i); // 大文字小文字ミックスされた状態
+        String question = questions.get(i);                         // 大文字小文字ミックスされた状態
         this.question = question;
-        convertedQuestion = getQuestion().toLowerCase(); // 全て小文字に変換したもの
+        convertedQuestion = getQuestion().toLowerCase();            // 全て小文字に変換したもの
         this.convertedQuestion = convertedQuestion;
         return question;
     }
 
-    // Convert its letters to underscores, and display
+    /**
+     * Convert question to underscore, and display
+     */
     public  void displayQuestion() {
-        int length = getQuestion().length(); // length of chosen city
+        int length = getQuestion().length();                        // length of chosen city
         for (int i = 0; i < length; i++) {
             char ch = question.charAt(i);
             String str = Character.toString(ch);
-            String str1 = " ";
-            if(str.equals(str1)) {
+            String str1 = "";
+            if(str.equals(str1)) {                                  // 文字がないところはunderbarでreplaceしない
                 currentGuess.replace(i, i + 1, " ");
             }
             currentGuess.replace(i, i + 1, "_");
@@ -112,57 +109,68 @@ public class Question {
         System.out.println(getCurrentGuess());
     }
 
-    //    take user's input, search for it in the city (-> boolean)
+    /**
+     * take user's input, return if the input is in the question
+     * @return boolean
+     */
     public boolean isInQuestion() {
         System.out.print("Guess a letter: ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         this.input = input; // user input
-        convertedInput = getInput().toLowerCase(); // user input all small letter
+        convertedInput = getInput().toLowerCase();                      // convert user input all small letter
         this.convertedInput = convertedInput;
-        convertedCharInput = convertedInput.charAt(0);  // charとして小文字で指定
+        convertedCharInput = convertedInput.charAt(0);                  // charとして小文字で指定
         this.convertedCharInput = convertedCharInput;
-        char charInput = input.charAt(0);  // user input(char ver)
+        char charInput = input.charAt(0);                               // user input(char ver)
         this.charInput = charInput;
 
         System.out.println("Your guess: " + getInput());
-        if (getConvertedQuestion().contains(getConvertedInput())) {  //小文字同士で比較
+        if (getConvertedQuestion().contains(getConvertedInput())) {     //小文字同士で比較
             return true;
         } else {
             return false;
         }
     }
 
-    // reveal correct letters and display (-> string)
+    /**
+     * reveal correct letters and display
+     * @return StringBuilder
+     */
     public StringBuilder displayGuess() {
         for (int i = 0; i < getQuestion().length(); i++) {
             if (getConvertedQuestion().charAt(i) == getConvertedCharInput()) {  //小文字で比較
                 char ch1 = getQuestion().charAt(i);
                 String str = String.valueOf(ch1);
-                currentGuess.replace(i, i + 1, str);    //入れ替えを本題のquestion stringのiばんめの文字持ってくる
+                currentGuess.replace(i, i + 1, str);                        //入れ替えにquestion stringのi番の文字持ってくる
             }
         }
         this.currentGuess = currentGuess;
         return currentGuess;
     }
 
-    // logic to keep track of wrong letters so they don't lose points
-
+    /**
+     * check if the input was previously mistaken one
+     * @return boolean
+     */
     public boolean isWrongBefore() {
-        for (int i = 0; i < getPrevioudWrongGuesses().size() ; i++) {
+        for (int i = 0; i < getPreviousWrongGuesses().size() ; i++) {
             if (getCharInput() == previousWrongGuesses.get(i)) {
                 return true;
             }
         }
-        //guessing the same char twice -> if user's input isn't in the city -> store
         return false;
     }
 
+    /**
+     * check it the game over or not
+     * @return boolean
+     */
     public boolean isOver(){
         if (! getCurrentGuess().toString().contains("_")){
             return true;
         }
-        else if(getPrevioudWrongGuesses().size() == 10){
+        else if(getPreviousWrongGuesses().size() == 10){
             return true;
         }
         return false;
